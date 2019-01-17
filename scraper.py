f60c60c9f944a8738fee120bcea46a1d71da1e3b
KP Kaiser <kirk@zothcorp.com>
2019-01-17 16:45:19 -0500

    Let's keep track of these files for now in CSV

--- scraper.py
+++ scraper.py
@@ -1,4 +1,6 @@
 import time
+import csv
+
 from bs4 import BeautifulSoup as bs
 from pytube import YouTube
 import requests
@@ -20,16 +22,25 @@ page = browser.html.encode()
 soup = bs(page, 'html.parser')
 vids = soup.findAll('a', attrs={'class':'yt-simple-endpoint style-scope ytd-video-renderer'})
 
+columns = ['id', 'url', 'title']
 videolist =[]
 for v in vids:
     try:
-        tmp = 'https://www.youtube.com' + v['href']
+        tmp = {'id': v['href'].split('?v=')[1],
+               'url': 'https://www.youtube.com' + v['href'],
+               'title': v['title']}
         videolist.append(tmp)
     except:
         continue
 
 print("Found and downloading %i videos" % len(videolist))
 
-for item in videolist:
-    print(item)
-    #YouTube(item).streams.first().download()
+try:
+    with open('out.csv', 'w', encoding='utf-8') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=columns)
+        writer.writeheader()
+        for item in videolist:
+            writer.writerow(item)
+except IOError:
+    print("couldn't open out.csv for writing %s" % IOError)
+
