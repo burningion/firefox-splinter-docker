e20b60775d25dd7ca53c81e1bc6f0ca1a37224ac
KP Kaiser <kirk@zothcorp.com>
2019-03-04 17:03:58 -0500

    Add interval that works better for ffmpeg

--- app/models.py
+++ app/models.py
@@ -4,6 +4,15 @@ import datetime
 
 db = SQLAlchemy()
 
+def get_sec(time_str):
+    h, m, s = time_str.split(':')
+    return int(h) * 3600 + int(m) * 60 + int(s)
+
+def getHrMinSec(seconds):
+    m, s = divmod(seconds, 60)
+    h, m = divmod(m, 60)
+    return '{:d}:{:02d}:{:02d}'.format(h, m, s)
+
 class Snippet(db.Model):
     __tablename__ = 'snippet'
     id = db.Column(db.Integer, primary_key=True)
@@ -18,15 +27,15 @@ class Snippet(db.Model):
     def __init__(self, filename, snippet_type, start, duration):
         self.filename = filename
         self.snippet_type = snippet_type
-        self.start = start
-        self.duration = duration
+        self.start = datetime.timedelta(seconds=get_sec(start))
+        self.duration = datetime.timedelta(seconds=get_sec(duration))
 
     def serialize(self):
         return {'id': self.id,
                 'filename': self.filename,
                 'snippet_type': self.snippet_type,
-                'duration': str(self.duration),
-                'start': str(self.duration),
+                'duration': getHrMinSec(self.duration.seconds),
+                'start': getHrMinSec(self.start.seconds),
                 'video': self.video.title
         }
 
